16a2b62e98f4710ee2bd5533dda7de6bc05d941a
candaceclaireee <mercadocandaceclaire@gmail.com>
2018-08-14 00:31:00 +0800

    Added GUI and Logic

--- src/main/Element.java
+++ src/main/Element.java
@@ -2,10 +2,11 @@ package main;
 
 public class Element {
     private String type;
+    private int location;
 
-    public Element(String type){
+    public Element(String type, int location){
         setType(type);
-
+        setLocation(location); //0 is earth, 1 is mars
     }
 
     public String getType() {
@@ -14,5 +15,13 @@ public class Element {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
     }
 }
